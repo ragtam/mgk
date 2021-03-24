0f75b29e6184c29d05257a81a698e0850c8e9388
m gb <mag190033@gmail.com>
2021-03-24 09:11:39 +0100

    transitions cd

--- src/home-screen/Home.svelte
+++ src/home-screen/Home.svelte
@@ -1,82 +1,12 @@
 <script>
-	import Rellax from 'rellax';
-	import anime from 'animejs/lib/anime.es.js';
-
 	import ManBehindTheDesk from './Man-Behind-The-Desk.svelte';
+	import { animateOnEntry, setUpParallaxEffect, setUpScrollAnimation } from './home.js';
 
 	setTimeout(() => {
-		setUpInitialAnimationEffect();
+		animateOnEntry();
 		setUpParallaxEffect();
-		setUpBackgroundColorChangeOnScroll();
+		setUpScrollAnimation();
 	}, 0);
-
-	function setUpInitialAnimationEffect() {
-		const t = anime.timeline({
-			easing: 'easeOutExpo',
-			duration: 750,
-		});
-		t.add({
-			targets: '.intro-overlay div',
-			opacity: 1,
-			delay: anime.stagger(100),
-		});
-		t.add(
-			{
-				targets: '.background-overlay',
-				opacity: 1,
-			},
-			'-=400'
-		);
-		t.add(
-			{
-				targets: '.intro-overlay div',
-				opacity: 0,
-				delay: anime.stagger(100),
-			},
-			'-=900'
-		);
-		t.add(
-			{
-				targets: '.image',
-				translateX: ['100%', '0'],
-				easing: 'spring(1, 80, 10, 0)',
-			},
-			'-=900'
-		);
-	}
-
-	function setUpParallaxEffect() {
-		setTimeout(() => {
-			new Rellax('.rellax');
-		}, 0);
-	}
-
-	function setUpBackgroundColorChangeOnScroll() {
-		const overlayAnimation = anime({
-			targets: '.overlay',
-			easing: 'easeInOutQuad',
-			opacity: 1,
-			autoplay: false,
-			duration: 500,
-		});
-
-		const observer = new IntersectionObserver(
-			(entries) => {
-				entries.forEach((entry) => {
-					if (!entry.isIntersecting) {
-						overlayAnimation.play();
-					} else {
-						overlayAnimation.seek(0);
-					}
-				});
-			},
-			{ threshold: 0.5 }
-		);
-
-		const target = document.querySelector('.sentinel');
-
-		observer.observe(target);
-	}
 </script>
 
 <div class="home row overflow-hidden gx-0">
@@ -95,14 +25,14 @@
 	<div class="sentinel" />
 	<div class="col-12 col-lg-4 d-flex align-items-center min-height-50vh">
 		<div class="mx-5 position-absolute">
-			<div class="rellax" data-rellax-speed="-2" data-rellax-xs-speed="0">
+			<div id="text" class="rellax" data-rellax-speed="-2" data-rellax-xs-speed="0">
 				<h1 class="display-4 text-muted rellax">Hello!</h1>
 				<h1 class="display-1 text-white rellax">I'm Mateusz</h1>
 				<h1 class="display-6 text-secondary rellax">Front End Developer struggling with CSS.</h1>
 			</div>
 		</div>
 	</div>
-	<div class="col-12 col-lg-8 d-flex justify-content-end max-height-50vh position-relative image">
+	<div id="image" class="col-12 col-lg-8 d-flex justify-content-end position-relative">
 		<div class="align-self-end">
 			<ManBehindTheDesk />
 		</div>
@@ -128,9 +58,6 @@
 		height: 100vh;
 		transition: background-color 1s;
 		opacity: 0;
-	}
-
-	.image {
 	}
 
 	.overlay {
---
+++ src/home-screen/home.js
@@ -0,0 +1,76 @@
+import Rellax from 'rellax';
+import anime from 'animejs/lib/anime.es.js';
+
+export function animateOnEntry() {
+    const t = anime.timeline({
+        easing: 'easeOutExpo',
+        duration: 750,
+    });
+    t.add({
+        targets: '.intro-overlay div',
+        opacity: 1,
+        delay: anime.stagger(100),
+    });
+    t.add(
+        {
+            targets: '.background-overlay',
+            opacity: 1,
+        },
+        '-=400'
+    );
+    t.add(
+        {
+            targets: '.intro-overlay div',
+            opacity: 0,
+            delay: anime.stagger(100),
+        },
+        '-=900'
+    );
+    t.add(
+        {
+            targets: '#image',
+            translateX: ['200', '0'],
+            opacity: [0, 1],
+            easing: 'spring(1, 80, 10, 0)',
+        },
+        '-=900'
+    );
+    t.add(
+        {
+            targets: '#text',
+            opacity: [0, 1],
+        },
+        '-=1100'
+    );
+}
+
+export function setUpParallaxEffect() {
+    new Rellax('.rellax');
+}
+
+export function setUpScrollAnimation() {
+    const overlayAnimation = anime({
+        targets: '.overlay',
+        easing: 'easeInOutQuad',
+        opacity: 1,
+        autoplay: false,
+        duration: 500,
+    });
+
+    const observer = new IntersectionObserver(
+        (entries) => {
+            entries.forEach((entry) => {
+                if (!entry.isIntersecting) {
+                    overlayAnimation.play();
+                } else {
+                    overlayAnimation.seek(0);
+                }
+            });
+        },
+        { threshold: 0.5 }
+    );
+
+    const target = document.querySelector('.sentinel');
+
+    observer.observe(target);
+}
