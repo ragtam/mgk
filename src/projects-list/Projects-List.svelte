2a16a2d625c34a8f612319e348129c71f0d42825
m gb <mag190033@gmail.com>
2021-04-16 15:37:48 +0200

    firebase ci added

--- src/projects-list/Projects-List.svelte
+++ src/projects-list/Projects-List.svelte
@@ -5,44 +5,31 @@
 <div>
 	<section class="project-list__header--sticky">
 		<div class="row">
-			<div class="col-12 bg-color" style="height: 3vh" />
+			<div class="col-12 bg-color vhs-material" style="height: 3vh" />
 		</div>
 		<div class="row">
-			<div class="col-5 bg-color" style="height: 6vh">My Projects</div>
-			<div class="col-1 bg-color" style="height: 6vh; clip-path: polygon(0% 0%,0% 100%,100% 0%);" />
+			<div class="col-5 bg-color vhs-material" style="height: 6vh">My Projects</div>
+			<div class="col-1 bg-color vhs-material" style="height: 6vh; clip-path: polygon(0% 0%,0% 100%,100% 0%);" />
 		</div>
 	</section>
 
-	<section
-		class="bg-color"
-		style="margin-top: -6vh; display: flex; justify-content: center; padding: 10vh 0; position: relative; z-index: 99;"
-	>
-		<div style="max-width: 50%; height: 20vh;">
-			<!-- <PortfolioFolder /> -->
-		</div>
-	</section>
-
-	<!-- <div class="bg-footer">
-		<img src="/assets/project-item-bottom.svg" />
-	</div> -->
-
-	<div class="scrollable-item" style="z-index: 3; bottom: 1.6rem">
-		<div class="test">
+	<div class="scrollable-item" style="z-index: 3; bottom: 18px">
+		<div class="scrollable-item__clip-path">
 			<ProjectItem />
 		</div>
 	</div>
-	<div class="scrollable-item" style="z-index: 2; bottom: .8rem">
-		<div class="test">
+	<div class="scrollable-item" style="z-index: 2; bottom: 10px">
+		<div class="scrollable-item__clip-path">
 			<ProjectItem />
 		</div>
 	</div>
 	<div class="scrollable-item" style="z-index: 1;">
-		<div class="test">
+		<div class="scrollable-item__clip-path">
 			<ProjectItem />
 		</div>
 	</div>
 
-	<div class="list-footer" />
+	<div class="list-footer vhs-material" />
 </div>
 
 <svg width="0" height="0" viewBox="0 0 478 374" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -62,12 +49,14 @@
 	.list-footer {
 		position: sticky;
 		bottom: 0;
-		height: 30vh;
-		margin-top: -30vh;
+		height: 100vh;
+	}
+
+	.vhs-material {
 		background-image: url('/assets/vhs-plastic.jpg');
 	}
 
-	.test {
+	.scrollable-item__clip-path {
 		clip-path: url(#svgPath);
 	}
 
@@ -77,8 +66,7 @@
 		height: 100vh;
 		position: sticky;
 		width: 100%;
-		/* box-shadow: 0 6px 20px 0 rgba(0, 0, 0, 0.19); */
-		filter: drop-shadow(-1px 6px 3px rgba(50, 50, 0, 0.5));
+		filter: drop-shadow(-1px 6px 3px rgba(131, 131, 129, 0.5));
 	}
 
 	.bg-color {
--- src/projects-list/project-item/Project-Item.svelte
+++ src/projects-list/project-item/Project-Item.svelte
@@ -1,30 +1,36 @@
 <section class="project-item pt-5">
-	<!-- <div class="bg-parent">
-		<div class="bg-stripes">
-			<div style="background-color: #FFBB00" />
-			<div style="background-color: #FF5A00" />
-			<div style="background-color: #FF002C" />
-			<div style="background-color: #AF00D0" />
-			<div style="background-color: #6300BA" />
-		</div>
-	</div> -->
-
-	<h1 class="display-5 text-end pt-5">Hackathon B-Hack</h1>
-	<div class="project-item__container row">
-		<div class="row project-item__content">
-			<div class="col-12 col-md-7 d-flex px-5">
-				<img class="align-self-center" style="width: 100%" src="/assets/hackathon.jpg" alt="" />
-			</div>
-			<div class="col-12 col-md-5">
-				<p class="mt-5">
-					Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to
-					twinkle. By the same illusion which lifts the horizon of the sea to the level of the spectator on a hillside, the sable
-					cloud beneath was dished out
-				</p>
-
-				<div class="d-grid mt-5">
-					<button class="btn btn-primary" style="background-color: #2e2e2e" type="button">Go to storybook</button>
+	<div style="position: absolute;width: 100%;height: 100%;">
+		<div style="height: 100%;" class="row">
+			<div class="col-1 col-sm-1 offset-sm-1" style="padding-right: 0">
+				<div class="bg-stripes">
+					<div style="background-color: #FFBB00" />
+					<div style="background-color: #FF5A00" />
+					<div style="background-color: #FF002C" />
+					<div style="background-color: #AF00D0" />
+					<div style="background-color: #6300BA" />
 				</div>
+			</div>
+		</div>
+	</div>
+
+	<h1 class="display-5 text-end pt-5 col-12 col-sm-11">Hackathon B-Hack</h1>
+	<div class="row project-item__content">
+		<div class="col-12 col-md-7 d-flex px-5">
+			<div class="row">
+				<div class="col-12 col-lg-10 d-flex align-items-center">
+					<img class="align-self-center" style="width: 100%" src="/assets/hackathon.jpg" alt="" />
+				</div>
+			</div>
+		</div>
+		<div class="col-10 offset-1 offset-sm-0 col-md-4">
+			<p class="mt-5">
+				Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to
+				twinkle. By the same illusion which lifts the horizon of the sea to the level of the spectator on a hillside, the sable
+				cloud beneath was dished out
+			</p>
+
+			<div class="d-grid mt-5">
+				<button class="btn btn-primary" style="background-color: #2e2e2e" type="button">Go to storybook</button>
 			</div>
 		</div>
 	</div>
@@ -35,30 +41,9 @@
 		background-color: #f9f1e4;
 	}
 
-	.sticky-header {
-		width: 100%;
-		position: sticky;
-		top: 3vh;
-		z-index: 0;
-	}
-
-	.project-item__container {
-		position: relative;
-		/* background-image: url('/assets/project-item-bottom.svg');
-		background-repeat: no-repeat;
-		background-position: right bottom; */
-	}
-
 	.project-item__content {
+		position: relative;
 		min-height: 80vh;
-	}
-
-	.bg-parent {
-		position: absolute;
-		overflow: hidden;
-		height: 100%;
-		width: 10rem;
-		top: 0;
 	}
 
 	.bg-stripes {
