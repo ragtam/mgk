0b710cdf50d1ce967980d0edf9c478402da238d2
m gb <mag190033@gmail.com>
2021-04-25 14:56:57 +0200

    my project header text color update

--- src/my-projects-header/My-Projects-Header.svelte
+++ src/my-projects-header/My-Projects-Header.svelte
@@ -2,37 +2,66 @@
 	import anime from 'animejs/lib/anime.es';
 	import { onMount } from 'svelte';
 
+	let isSectionVisible = false;
+
 	onMount(() => {
 		setTimeout(() => {
-			var animation = anime({
-				targets: '.my-projects-header .secondary-text',
-				width: ['0', '100%'],
-				easing: 'easeOutInBounce',
-				duration: 1000,
-				autoplay: false,
-			});
-
-			const observer = new IntersectionObserver(
-				(entries, observer) => {
-					entries.forEach((entry) => {
-						if (entry.isIntersecting) {
-							animation.play();
-							observer.unobserve(entry.target);
-						}
-					});
-				},
-				{ threshold: 0.8 }
-			);
-
-			const target = document.querySelector('.my-projects-header');
-			observer.observe(target);
+			const animation = getAnimationDefinition();
+			const willChangeObserver = getWillChangeObserver();
+			const animationObserver = getAnimationObserver(animation);
+			const target = getElementToObserve();
+			animationObserver.observe(target);
+			willChangeObserver.observe(target);
 		});
 	});
+
+	function getAnimationDefinition() {
+		return anime({
+			targets: '.my-projects-header .secondary-text',
+			width: ['0', '100%'],
+			easing: 'easeOutInBounce',
+			duration: 1000,
+			autoplay: false,
+		});
+	}
+
+	function getWillChangeObserver() {
+		return new IntersectionObserver(
+			(entries) => {
+				entries.forEach((entry) => {
+					if (entry.isIntersecting) {
+						isSectionVisible = true;
+					} else {
+						isSectionVisible = false;
+					}
+				});
+			},
+			{ threshold: 0.1 }
+		);
+	}
+
+	function getAnimationObserver(animation) {
+		return new IntersectionObserver(
+			(entries, observer) => {
+				entries.forEach((entry) => {
+					if (entry.isIntersecting) {
+						animation.play();
+						observer.unobserve(entry.target);
+					}
+				});
+			},
+			{ threshold: 0.8 }
+		);
+	}
+
+	function getElementToObserve() {
+		return document.querySelector('.my-projects-header');
+	}
 </script>
 
 <div class="my-projects-header">
-	<h1 class="main-text display-1">
-		<div class="secondary-text">My Projects</div>
+	<h1 class="main-text display-1 py-4 px-2">
+		<div class="secondary-text py-4 px-2 {isSectionVisible ? 'secondary-text__width-will-change' : ''}">My Projects</div>
 		My Projects
 	</h1>
 </div>
@@ -51,9 +80,9 @@
 
 	.my-projects-header .main-text {
 		position: sticky;
-		top: 1rem;
+		top: 1.3rem;
 		color: #333333;
-		/* -webkit-text-stroke: 1px #d9003a; */
+		-webkit-text-stroke: 1px black;
 		font-weight: bolder;
 		text-transform: uppercase;
 	}
@@ -66,7 +95,12 @@
 		top: 0;
 		left: 0;
 		color: #595959;
-		-webkit-text-stroke: 1px #000000;
+		-webkit-text-stroke: 1px #feb603;
 		overflow: hidden;
+		border-right: 3px solid #feb603;
+	}
+
+	.secondary-text__width-will-change {
+		will-change: width;
 	}
 </style>
