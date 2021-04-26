f0fc279f1333f3481c1b4868cf7a8e8583a97f87
m gb <mag190033@gmail.com>
2021-04-26 22:40:21 +0200

    card update

--- src/my-projects/My-Project.svelte
+++ src/my-projects/My-Project.svelte
@@ -1,7 +1,7 @@
 <div class="my-project">
 	<div class="row">
 		<div class="col-4 section-left">
-			<div class="col-12 col-sm-8 offset-sm-1 d-flex justify-content-between align-self-center">
+			<div class="col-12 col-sm-8 offset-sm-1 d-flex justify-content-between align-self-top pt-5 mt-5">
 				<p class="text-muted font-weight-bold text-uppercase">| tech stack</p>
 				<ul class="text-dark ">
 					<li>Vue</li>
@@ -10,28 +10,32 @@
 					<li>Storybook</li>
 				</ul>
 			</div>
-			<div class="col-11 offset-1  d-flex flex-column align-self-center">
+			<div class="col-11 offset-1  d-flex flex-column align-self-top pt-5 mt-5">
 				<p class="text-muted font-weight-bold text-uppercase">| name</p>
-				<h6 class="display-2" style="color: #f83d46;">Hackathon B-Hack</h6>
+				<h6 class="display-2" style="color: #f83d46;">Hackathon</h6>
 			</div>
 		</div>
 		<div class="col-4 d-flex align-items-center">
 			<div class="vhs-tape mx-0 mx-lg-5">
 				<div class="container">
-					<h1 class="h2 font-weight-light mt-5 text-center">Hackathon B-Hack Barbórka</h1>
-					<p>
+					<h1 class="h5 font-weight-bold text-uppercase mt-5 pb-0 mb-0 text-center">Hackathon B-Hack</h1>
+					<h1 class="display-3 text-center mb-5">Barbórka 4.0</h1>
+					<p class="col-9 pt-3 text-right">
 						Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased
 						to twinkle. By the same illusion which lifts the horizon of.
 					</p>
+					<button class="btn btn-secondary mt-5 col-8 offset-2" style="background-color: #4a4a4a" type="button"
+						>Check project`s storybook page</button
+					>
 				</div>
 			</div>
 		</div>
 		<div class="col-4 section-right">
-			<div class="col-12 col-sm-8 offset-sm-1 d-flex justify-content-between align-self-center">
+			<div class="col-12 col-sm-8 offset-sm-1 d-flex justify-content-between align-self-top pt-5 mt-5">
 				<p class="text-muted font-weight-bold text-uppercase">| date</p>
 				<p class="text-dark">11.2021</p>
 			</div>
-			<div class="col-11 offset-1 d-flex flex-column align-self-center">
+			<div class="col-11 offset-1 d-flex flex-column align-self-top pt-5 mt-5">
 				<!-- <p class="text-muted font-weight-bold text-uppercase">NAME/</p>
 				<h6 class="display-2" style="color: #f83d46;">Hackathon B-Hack</h6> -->
 			</div>
