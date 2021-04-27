5184e94065a879ac469a09e58a3e0cc1378f17d7
m gb <mag190033@gmail.com>
2021-04-27 15:06:29 +0200

    cd

--- src/my-projects/My-Project.svelte
+++ src/my-projects/My-Project.svelte
@@ -25,7 +25,12 @@
 <div class="my-project">
 	<div class="row">
 		<div class="position-absolute background-section">
-			<div class="col-2 offset-10 col-sm-5 offset-sm-7" />
+			<div
+				class="col-2 offset-10 col-sm-5 offset-sm-7 rellaxx"
+				data-rellax-speed="-1"
+				data-rellax-mobile-speed="1"
+				data-rellax-xs-speed="1"
+			/>
 		</div>
 		<div
 			class="col-12 col-sm-4 order-1 order-sm-1 section-left rellaxx"
@@ -48,15 +53,15 @@
 			</div>
 		</div>
 		<div class="col-12 col-sm-4 order-3 order-sm-2 d-flex align-items-end">
-			<div class="vhs-tape mx-0 mx-lg-5 rellaxx" data-rellax-speed="2" data-rellax-mobile-speed="0" data-rellax-xs-speed="0">
+			<div class="vhs-tape mx-0 mx-lg-5 rellaxx" data-rellax-speed="2" data-rellax-mobile-speed="-1" data-rellax-xs-speed="-1">
 				<div class="container">
 					<h1 class="h5 font-weight-bold text-uppercase mt-5 pb-0 mb-0 text-center">Hackathon B-Hack</h1>
 					<h1 class="display-3 text-center mb-5">Barbórka 4.0</h1>
-					<p class="col-9 pt-3 text-right">
+					<p class="col-10 pt-3 text-right">
 						Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased
 						to twinkle. By the same illusion which lifts the horizon of.
 					</p>
-					<button class="btn btn-secondary mt-5 col-8 offset-2" style="background-color: #4a4a4a" type="button"
+					<button class="btn btn-secondary mt-5 col-8 offset-1" style="background-color: #4a4a4a" type="button"
 						>Check project`s storybook page</button
 					>
 				</div>
@@ -72,7 +77,6 @@
 				<p class="text-muted font-weight-bold text-uppercase">| date</p>
 				<p class="text-dark text-sm-left">11.2021</p>
 			</div>
-			<div class="col-11 offset-1 d-flex flex-column align-self-top pt-5 mt-5" />
 		</div>
 	</div>
 </div>
@@ -89,7 +93,7 @@
 	}
 
 	.vhs-tape {
-		height: 60vh;
+		height: 65vh;
 		width: 100%;
 		position: relative;
 		box-shadow: 3px 3px 5px 6px rgb(0 0 0 / 20%);
@@ -114,6 +118,6 @@
 
 	.background-section > div {
 		background: linear-gradient(180deg, #f83d46 20%, #4a4a4a 0%);
-		height: 100%;
+		height: 80vh;
 	}
 </style>
