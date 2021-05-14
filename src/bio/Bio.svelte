a20190484baecb2557d3e8ee48196ac27194dd9f
m gb <mag190033@gmail.com>
2021-05-14 12:45:50 +0200

    cd

--- src/bio/Bio.svelte
+++ src/bio/Bio.svelte
@@ -1,46 +1,47 @@
-<div class="bio__container">
-	<div class="bio d-flex align-items-center">
-		<div class="container">
-			<div class="bio__frame">
-				<div class="container">
-					<div class="row">
-						<div class="col-11">
-							<div class="row">
-								<div class="col-12">
-									<h1 class="h5 text-dark font-weight-light text-right text-uppercase">Let`s get in touch.</h1>
-								</div>
-							</div>
-							<div class="row" style="min-height: 50vh;">
-								<div class="col-12 d-flex align-items-center text-secondary"><h1 class="display-2">About me</h1></div>
-								<div class="col-12 col-sm-6 my-6">
-									<p class="text-dark">
-										Currently I am doing <b>Angular</b> at <b>Credit Suisse</b>. Previously worked at <b>Volvo</b>,
-										creating and maintaining
-										<b>Angular</b> applications with some <b>.NET</b> tasks too. Visit my linkedin profile for more details.
-									</p>
-								</div>
-								<div class="col-12 col-sm-6">
-									<p class="text-dark mb-5">
-										Off work I explore other frameworks: <b>Vue</b> or quite recently <b>Svelte</b>. I am making my
-										first steps in <b>web design</b>, this website being an example.
-									</p>
-								</div>
+<div class="bio d-flex align-items-center">
+	<div class="container">
+		<div class="bio__frame">
+			<div class="container">
+				<div class="row">
+					<div class="col-12 col-sm-11">
+						<div class="row">
+							<div class="col-12">
+								<h1 class="h5 text-dark font-weight-light text-right text-uppercase">Let`s get in touch.</h1>
 							</div>
 						</div>
-						<div class="col-1 d-flex flex-column align-items-center" style="background-color: #d62828">
-							<a href="https://github.com/ragtam" target="_blank" class="btn rounded-circle" role="button"
-								><i class="bi bi-google" /></a
-							>
-							<a
-								href="https://www.linkedin.com/in/mateusz-garbaciak-b50b20194/"
-								target="_blank"
-								class="btn rounded-circle"
-								role="button"><i class="bi bi-linkedin" /></a
-							>
-							<a href="https://github.com/ragtam" target="_blank" class="btn rounded-circle" role="button"
-								><i class="bi bi-github" /></a
-							>
+						<div class="row" style="min-height: 50vh;">
+							<div class="col-12 d-flex align-items-center text-secondary">
+								<h1 class="display-2 d-none d-sm-block">About me</h1>
+								<h1 class="display-4 d-block d-sm-none">About me</h1>
+							</div>
+							<div class="col-12 col-sm-6 my-6">
+								<p class="text-dark">
+									Currently I am doing <b>Angular</b> at <b>Credit Suisse</b>. Previously worked at <b>Volvo</b>, creating
+									and maintaining
+									<b>Angular</b> applications with some <b>.NET</b> tasks too. Visit my linkedin profile for more details.
+								</p>
+							</div>
+							<div class="col-12 col-sm-6">
+								<p class="text-dark mb-5">
+									Off work I explore other frameworks: <b>Vue</b> or quite recently <b>Svelte</b>. I am making my first
+									steps in <b>web design</b>, this website being an example.
+								</p>
+							</div>
 						</div>
+					</div>
+					<div class="col-12 col-sm-1 d-flex flex-row flex-sm-column align-items-center" style="background-color: #003049">
+						<a href="https://github.com/ragtam" target="_blank" class="btn rounded-circle" role="button"
+							><i class="bi bi-google" /></a
+						>
+						<a
+							href="https://www.linkedin.com/in/mateusz-garbaciak-b50b20194/"
+							target="_blank"
+							class="btn rounded-circle"
+							role="button"><i class="bi bi-linkedin" /></a
+						>
+						<a href="https://github.com/ragtam" target="_blank" class="btn rounded-circle" role="button"
+							><i class="bi bi-github" /></a
+						>
 					</div>
 				</div>
 			</div>
@@ -49,10 +50,6 @@
 </div>
 
 <style>
-	.bio__container {
-		min-height: 100vh;
-	}
-
 	.bio {
 		box-shadow: inset 0px 9px 6px 1px rgb(0 0 0 / 20%);
 		position: fixed;
@@ -61,13 +58,18 @@
 		right: 0;
 		bottom: 0;
 		z-index: -2;
-		background: repeating-radial-gradient(circle, #eae2b7, #eae2b7 40px, #fcbf49 40px, #fcbf49 80px);
+		background: repeating-radial-gradient(circle, #eae2b7, #eae2b7 40px, #d62828 40px, #d62828 80px);
 	}
 
 	.bio__frame {
-		background-color: #f77f00;
-		border: 3px solid #d62828;
+		background-color: #eae2b7;
+		border: 3px solid #003049;
 		border-radius: 10px;
 		box-shadow: 4px 4px 5px 6px rgb(0 0 0 / 20%);
+	}
+
+	a {
+		color: #eae2b7;
+		font-size: 2rem;
 	}
 </style>
--- src/footer/Footer.svelte
+++ src/footer/Footer.svelte
@@ -1,7 +1,10 @@
-<div class="footer">footer</div>
+<div class="footer">
+	<h1 class="text-muted">Footer</h1>
+</div>
 
 <style>
 	.footer {
+		margin-top: 100vh;
 		box-shadow: 0px -8px 6px 1px rgb(0 0 0 / 20%);
 		min-height: 50vh;
 		background-color: #003049;
