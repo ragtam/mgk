442d2628a463ab86b9610974ca0768908a10a21b
m gb <mag190033@gmail.com>
2021-06-13 15:54:41 +0200

    added jest, npm-stats service update start

--- src/services/npm-stats.js
+++ src/services/npm-stats.js
@@ -1,6 +1,31 @@
 //https://github.com/npm/registry/blob/master/docs/download-counts.md
 
+import dayjs from 'dayjs';
+
 const NPM_API_URL = 'https://api.npmjs.org/downloads/range';
+const MAX_MONTH_RANGE = 18;
+
+export function prepareBatchCallsRanges(dateFrom, dateTo) {
+    const batchArray = [];
+    return getBatches(dateFrom, dateTo, batchArray);
+}
+
+function getBatches(dateFrom, dateTo, array) {
+    const diff = getMonthsDifference(dateFrom, dateTo);
+    if(diff < MAX_MONTH_RANGE) {
+        array.push( { from: dateFrom, to: dateTo } );
+        return array;
+    } else {
+        const newDateFrom = dayjs(dateFrom).add(MAX_MONTH_RANGE, 'month').toDate();
+        const batch = { from: dateFrom, to: newDateFrom };
+        array.push(batch);
+        return getBatches( newDateFrom, dateTo, array );
+    }
+}
+
+function getMonthsDifference(dateFrom, dateTo) {
+    return Math.abs(dayjs(dateFrom).diff(dateTo, "month"));
+}
 
 export async function fetchNPMPackageDownloadCountSince(startDate, packageName) {
     const stats = await getStatistics(formatDate(startDate), formatDate(new Date()), packageName);
---
+++ src/services/npm-stats.spec.js
@@ -0,0 +1,30 @@
+import { prepareBatchCallsRanges } from './npm-stats';
+import dayjs from 'dayjs';
+
+function getDate(dateString) {
+    return dayjs(dateString).toDate();
+}
+
+it('should return one batch if date difference is less than 18 months', () => {
+    const from = getDate('2020-05-25');
+    const to = getDate('2021-05-25');
+
+    const res = prepareBatchCallsRanges(from, to);
+
+    expect(res.length).toEqual(1);
+    expect(res[0].from).toEqual(from);
+    expect(res[0].to).toEqual(to);
+});
+
+it('should return two batches if date difference is 19 months', () => {
+    const from = getDate('2019-06-25');
+    const to = getDate('2021-01-25');
+
+    const res = prepareBatchCallsRanges(from, to);
+
+    expect(res.length).toEqual(2);
+    expect(res[0].from).toEqual(from);
+    expect(res[0].to).toEqual(getDate('2020-12-25'));
+    expect(res[0].from).toEqual(getDate('2020-12-26'));
+    expect(res[1].to).toEqual(to);
+});
