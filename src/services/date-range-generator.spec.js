6320d4ecbf47df98a54c5e9185ea20eabb0e69fb
m gb <mag190033@gmail.com>
2021-06-14 11:30:00 +0200

    date ranges done

--- src/services/date-range-generator.spec.js
+++ src/services/date-range-generator.spec.js
@@ -1,4 +1,4 @@
-import { prepareBatchCallsRanges } from './npm-stats';
+import DateRangeGenerator from './date-range-generator';
 import dayjs from 'dayjs';
 
 function getDate(dateString) {
@@ -9,7 +9,7 @@ it('should return one batch if date difference is less than 18 months', () => {
     const from = getDate('2020-05-25');
     const to = getDate('2021-05-25');
 
-    const res = prepareBatchCallsRanges(from, to);
+    const res = new DateRangeGenerator(18).generate(from, to);
 
     expect(res.length).toEqual(1);
     expect(res[0].from).toEqual(from);
@@ -20,11 +20,11 @@ it('should return two batches if date difference is 19 months', () => {
     const from = getDate('2019-06-25');
     const to = getDate('2021-01-25');
 
-    const res = prepareBatchCallsRanges(from, to);
+    const res = new DateRangeGenerator(18).generate(from, to);
 
     expect(res.length).toEqual(2);
     expect(res[0].from).toEqual(from);
     expect(res[0].to).toEqual(getDate('2020-12-25'));
-    expect(res[0].from).toEqual(getDate('2020-12-26'));
+    expect(res[1].from.toISOString()).toEqual(getDate('2020-12-26').toISOString());
     expect(res[1].to).toEqual(to);
 });
